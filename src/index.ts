5a1b5cd41911d34689ae89020e17387f8dfed36c
Ann 杏 Kilzer <akilzer@gmail.com>
2022-07-26 00:54:02 +0900

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+import fs from 'fs'
+import path from 'path'
 
-import { schema } from "./schema";
-export const server = new ApolloServer({
-  schema,
+const names = [
+  {
+    en: 'Ann',
+    ja: '杏',
+  },
+  {
+    en: 'LaShawn',
+    ja: 'ラシャーン',
+  },
+  {
+    en: 'Philip',
+    ja: 'フィリップ',
+  },
+];
+
+const resolvers = {
+  Query: {
+    name: () => names,
+  },
+};
+
+// todo add error handling
+// and find a less hacky way of doing this
+const typeString = fs.readFileSync(path.join(__dirname, './typeDefs/schema.graphql'),"utf-8")
+const typeDefs = gql`${typeString}`
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
 });
 
 const port = 3000;
