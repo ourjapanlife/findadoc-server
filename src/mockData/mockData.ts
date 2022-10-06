cf99f1b280dfb04b729c0e3498d78a806d29653b
LaShawn <lts001@gmail.com>
2022-10-06 21:25:24 +0900

    update mock data and setup queries
    Co-authored-by: Ann Kilzer akilzer@gmail.com
    Co-authored-by: Philip Ermish philipermish@gmail.com

--- src/mockData/mockData.ts
+++ src/mockData/mockData.ts
@@ -1,65 +1,22 @@
-export const names = [
-  {
-    id: "1",
-    lastName: "Kilzer",
-    firstName: "Ann",
-    middleName: "",
-    language: "ENGLISH",
-  },
-  {
-    id: "1",
-    lastName: "キルザー",
-    firstName: "杏",
-    middleName: "",
-    language: "JAPANESE",
-  },
-  {
-    id: "2",
-    lastName: "Toyoda",
-    firstName: "LaShawn",
-    middleName: "T",
-    language: "ENGLISH",
-  },
-  {
-    id: "2",
-    lastName: "豊田",
-    firstName: "ラシァン",
-    middleName: "ティ",
-    language: "JAPANESE",
-  },
-  {
-    id: "3",
-    lastName: "Ermish",
-    firstName: "Philip",
-    middleName: "Michael",
-    language: "ENGLISH",
-  },
-  {
-    id: "3",
-    lastName: "アーミッシュ",
-    firstName: "フィリップ",
-    middleName: "マイケル",
-    language: "JAPANESE",
-  },
-];
+// locale will be used for the site's language settings
 
-export const healthcareProfessionals = [
+// language is the spoken language
+
+const healthcareProfessionals = [
   {
     id: "1",
     names: [
       {
-        id: "3",
         lastName: "Ermish",
         firstName: "Philip",
         middleName: "Michael",
-        language: "ENGLISH",
+        locale: "en",
       },
       {
-        id: "3",
         lastName: "アーミッシュ",
         firstName: "フィリップ",
         middleName: "マイケル",
-        language: "JAPANESE",
+        locale: "ja",
       },
     ],
     spokenLanguages: ["JAPANESE", "ENGLISH"],
@@ -68,20 +25,37 @@ export const healthcareProfessionals = [
     id: "2",
     names: [
       {
-        id: "1",
         lastName: "Kilzer",
         firstName: "Ann",
         middleName: "",
-        language: "ENGLISH",
+        locale: "en",
       },
       {
-        id: "1",
         lastName: "キルザー",
         firstName: "杏",
         middleName: "",
-        language: "JAPANESE",
+        locale: "ja",
       },
     ],
     spokenLanguages: ["JAPANESE"],
   },
+  {
+    id: "3",
+    names: [
+      {
+        lastName: "Toyoda",
+        firstName: "LaShawn",
+        middleName: "T",
+        locale: "en",
+      },
+      {
+        lastName: "豊田",
+        firstName: "ラシァン",
+        middleName: "ティ",
+        locale: "ja",
+      },
+    ],
+  },
 ];
+
+export default healthcareProfessionals;
--- src/typeDefs/resolvers.ts
+++ src/typeDefs/resolvers.ts
@@ -1,24 +1,14 @@
-import { names, healthcareProfessionals } from "../mockData/mockData";
-
-// need to set the proper types for the params
+import healthcareProfessionals from "../mockData/mockData";
 
 const resolvers = {
   Query: {
-    names: () => names,
-    name: (_parent: any, args: any) => {
-      if (args.language !== undefined) {
-        return names.filter(
-          (person) => person.id === args.id && person.language === args.language
-        );
-      }
-      return names.filter((person) => person.id === args.id);
-    },
     healthcareProfessionals: () => healthcareProfessionals,
     healthcareProfessional: (_parent: any, args: any) => {
-      const doctor = healthcareProfessionals.filter(
+      const matchingResults = healthcareProfessionals.filter(
         (person) => person.id === args.id
       );
-      return doctor[0];
+      const firstResult = matchingResults.length ? matchingResults[0] : null;
+      return firstResult;
     },
   },
 };
