798d31ad8e9cd7f057ec34dc2580cde1d4de219d
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2022-10-07 21:16:13 +0900

    install codegen packages (#19)
    * install codegen packages and added gql typescript types generator
    
    Co-authored-by: Ann Kilzer <akilzer@gmail.com>
    Co-authored-by: Philip Ermish <philipermish@gmail.com>

---
+++ typesgeneratorconfig.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:3000/graphql",
+  generates: {
+    "src/typeDefs/gqlTypes.ts": {
+      plugins: ["typescript-resolvers"],
+    },
+  },
+  debug: true,
+};
+
+export default config;
