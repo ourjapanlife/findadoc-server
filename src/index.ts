443db82ddf462e6027d1b4efdc6ba30455454d28
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-18 21:26:44 +0900

    prisma checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 import { ApolloServer } from "apollo-server";
-import { Knex } from "knex";
-import { Model } from "objection";
-import knexConfig from "./database/knexfile";
 import loadSchema from "./schema";
 import resolvers from "./resolvers";
-
-// Initialize knex.
-const knex = Knex(knexConfig.development);
-
-// Bind all Models to a knex instance. If you only have one database in
-// your server this is all you have to do. For multi database systems, see
-// the Model.bindKnex() method.
-Model.knex(knex);
 
 const server = new ApolloServer({
   typeDefs: loadSchema(),
@@ -23,5 +12,4 @@ const port = 3000;
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
-  console.log(`🐘 Connected to postgres version ${pg.VERSION}`);
 });
