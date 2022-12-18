d5111823ba26c6a267226f01a74ac15a3adb0a81
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-18 18:08:47 +0900

    boilerplate for knex

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { ApolloServer } from "apollo-server";
+import knex from "knex";
 import loadSchema from "./schema";
 import resolvers from "./resolvers";
+
+// Set up Database
+const pg = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    port: 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+  searchPath: ["knex", "public"],
+  debug: process.env.NODE_ENV === "development", // debug logging for dev only
+});
 
 const server = new ApolloServer({
   typeDefs: loadSchema(),
@@ -12,4 +27,5 @@ const port = 3000;
 
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
+  console.log(`🐘 Connected to postgres version ${pg.VERSION}`);
 });
