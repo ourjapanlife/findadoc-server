df879c3e551bf106b5357da77487081271a30d81
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-28 19:49:27 +0900

    more seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 // Seed data for the database
 // https://www.prisma.io/docs/guides/database/seed-database
 import { PrismaClient } from '@prisma/client';
@@ -22,23 +23,89 @@ function loadCSVFromFile(filename: string, discardHeader = true) {
   return records;
 }
 
-async function main() {
+async function seedSpokenLanguages(verbose = false) {
+  const iso639Col = 0;
+  const enCol = 1;
+  const jaCol = 3;
+
   const spokenLanguages:string[][] = loadCSVFromFile('./prisma/seedData/spokenLanguages.csv');
 
-  // TODO: this depends on "magic values" -- refactor later to use headers to build objects
   spokenLanguages.forEach(async (language: string[]) => {
     const upserted = await prisma.spokenLanguage.upsert({
-      where: { iso639_3: language[0] },
+      where: { iso639_3: language[iso639Col] },
       update: {},
       create: {
-        iso639_3: language[0],
-        nameEn: language[1],
-        nameJa: language[3],
+        iso639_3: language[iso639Col],
+        nameEn: language[enCol],
+        nameJa: language[jaCol],
       },
     });
 
-    console.log(`Inserting ${upserted.nameEn}`);
+    if (verbose) {
+      // eslint-disable-next-line no-console
+      console.log(`Inserting ${upserted.nameEn} into SpokenLanguages`);
+    }
   });
+}
+
+async function seedSpecialties(verbose = false) {
+  const enCol = 0;
+  const jaCol = 1;
+
+  const specialties:string[][] = loadCSVFromFile('./prisma/seedData/specialties.csv');
+
+  let count = 0;
+  specialties.forEach(async (specialty: string[]) => {
+    const upserted = await prisma.specialty.upsert({
+      where: { id: count },
+      update: {},
+      create: {
+        nameEn: specialty[enCol],
+        nameJa: specialty[jaCol],
+      },
+    });
+
+    count++;
+
+    if (verbose) {
+      // eslint-disable-next-line no-console
+      console.log(`Inserting ${upserted.nameEn} into Specialties`);
+    }
+  });
+}
+
+async function seedDegrees(verbose = false) {
+  const enCol = 0;
+  const abbrCol = 1;
+  const jaCol = 2;
+
+  const degrees:string[][] = loadCSVFromFile('./prisma/seedData/degrees.csv');
+
+  let count = 0;
+  degrees.forEach(async (specialty: string[]) => {
+    const upserted = await prisma.degree.upsert({
+      where: { id: count },
+      update: {},
+      create: {
+        nameEn: degrees[enCol],
+        nameJa: degrees[jaCol],
+        abbreviation: degrees[abbrCol],
+      },
+    });
+
+    count++;
+
+    if (verbose) {
+      // eslint-disable-next-line no-console
+      console.log(`Inserting ${upserted.nameEn} into Degrees`);
+    }
+  });
+}
+
+async function main() {
+  const verbose = true;
+  await seedSpokenLanguages(verbose);
+  await seedSpecialties(verbose);
 }
 
 main()
