3265f6a12c67bf650eda0fa34ea5fce9dcc222a8
Ann 杏 Kilzer <akilzer@gmail.com>
2022-12-28 19:04:38 +0900

    seeding sorta working

---
+++ prisma/seed.ts
@@ -0,0 +1,55 @@
+// Seed data for the database
+// https://www.prisma.io/docs/guides/database/seed-database
+import { PrismaClient } from '@prisma/client';
+import { parse } from 'csv-parse/sync';
+import * as fs from 'fs';
+
+const prisma = new PrismaClient();
+
+// loads a CSV file from the filesystem, ready for parsing
+function loadCSVFromFile(filename: string) {
+  const input = fs.readFileSync(filename);
+
+  // Initialize the parser
+  const records = parse(input, {
+    delimiter: ',',
+  });
+  return records;
+}
+
+// TODO: read from CSV file and insert the values
+const spokenLanguages = loadCSVFromFile('./prisma/seedData/spokenLanguages.csv');
+console.log(spokenLanguages);
+
+async function main() {
+  const japanese = await prisma.spokenLanguage.upsert({
+    where: { iso639_3: 'ja' },
+    update: {},
+    create: {
+      iso639_3: 'ja',
+      nameEn: 'Japanese',
+      nameJa: '日本語',
+    },
+  });
+  const english = await prisma.spokenLanguage.upsert({
+    where: { iso639_3: 'en' },
+    update: {},
+    create: {
+      iso639_3: 'en',
+      nameEn: 'English',
+      nameJa: '英語',
+    },
+  });
+
+  console.log(japanese, english);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
