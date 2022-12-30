d19180f6c6e50bae091225e4ca5e103297ff5e42
LaShawn <lts001@gmail.com>
2022-12-30 20:56:44 +0900

    chore: wip

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,5 @@
 import crypto from 'crypto';
 import {
-  facilities,
   healthcareProfessionals,
   specialties as medicalField,
 } from './mockData/mockData';
@@ -73,16 +72,21 @@ const resolvers = {
 export default resolvers;
 
 // from Philip -
-// create similar methods for mutations in the services. Make sure they are abstracted from knowing anything about the API/gql.
-// validate our inputs. (make sure they're not empty if required, valid types such as number vs string, etc.) make sure types in our enums are only allowed like "english" in languages
+// create similar methods for mutations in the services.
+//    Make sure they are abstracted from knowing anything about the API/gql.
+// validate our inputs. (make sure they're not empty if required,
+//     valid types such as number vs string, etc.)
+//     make sure types in our enums are only allowed like "english" in languages
 // When we can call the db, make sure the service calls are async.
 // wrap the service call in a try catch.
-// Implement failure state for api calls. Invalid input, server error, etc.   We probably need error code so client knows how to handle it.
+// Implement failure state for api calls. Invalid input, server error, etc.
+//        We probably need error code so client knows how to handle it.
 // next step
 // create an auth service that can authenticate and authorize a user.
 // Set up our api queries and mutations to always check auth first.
 // Bonus credit
-// create a searchService that gets both facilities and professionals and has some mixture of results of the two.
+// create a searchService that gets both facilities and professionals
+//       and has some mixture of results of the two.
 // create a new query for more generalized search that isn't specific to facility/professional
 // next step
 // admin dashboard methods.
