75221771d2153d8325526bc7d2d26b62a345c38c
LaShawn <lts001@gmail.com>
2023-01-01 00:32:06 +0900

    chore: replace any with types

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -9,41 +9,50 @@ import {
   Language,
   HealthcareProfessional,
   Specialty,
+  PersonNameInput,
+  HealthcareProfessionalInput,
 } from './typeDefs/gqlTypes';
 
 const resolvers = {
   Query: {
-    facilities: (_parent: Array<Facility>, args: any) => {
-      console.log(args);
+    facilities: (_parent: Array<Facility>, args: {
+      specialty: string, location: string, spokenLanguage: string;
+    }) => {
       // TODO: add a validation step for incoming parameters
       const matchingFacilities = getFacilities(args.specialty, args.location, args.spokenLanguage);
 
       return matchingFacilities;
     },
-    facility: (_parent: any, args: any) => {
+    facility: (_parent: Facility, args: { id: string; }) => {
       // TODO: add a validation step for incoming parameters
       const matchingFacility = getFacilityById(args.id);
       return matchingFacility;
     },
-    healthcareProfessionals: (_parent: any, args: any) => {
+    healthcareProfessionals: (_parent: Array<HealthcareProfessional>, args: {
+      specialty: string,
+      spokenLanguage: Language
+    }) => {
       // TODO: add a validation step for incoming parameters
       const matchingProfessionals = getHealthcareProfessionals(args.specialty, args.spokenLanguage);
 
       return matchingProfessionals;
     },
-    healthcareProfessional: (_parent: any, args: any) => {
+    healthcareProfessional: (_parent: HealthcareProfessional, args: { id: string; }) => {
       // TODO: add a validation step for incoming parameters
       const matchingHealthcareProfessional = getHealthcareProfessionalById(args.id);
 
       return matchingHealthcareProfessional;
     },
-    specialties: (_parent: any, args: any) => {
+    specialties: (_parent: Array<Specialty>, args: {
+      specialty: string,
+      spokenLanguage: Language,
+    }) => {
       // TODO: add a validation step for incoming parameters
       const matchingSpecialties = getSpecialties(args.specialty, args.spokenLanguage);
 
       return matchingSpecialties;
     },
-    specialty: (_parent: any, args: any) => {
+    specialty: (_parent: Specialty, args: { id: string; }) => {
       // TODO: add a validation step for incoming parameters
       const matchingSpecialty = getSpecialtyById(args.id);
 
@@ -51,7 +60,14 @@ const resolvers = {
     },
   },
   Mutation: {
-    createHealthcareProfessional: (_parent: any, args: any) => {
+    createHealthcareProfessional: (_parent: HealthcareProfessionalInput, args: {
+      id: string,
+      names: Array<PersonNameInput>,
+      degrees: Array<Degree>,
+      spokenLanguages: Array<Language>,
+      specialties: Array<Specialty>,
+      acceptedInsuranceOptions: Array<Insurance>
+    }) => {
       const id = crypto.randomUUID();
 
       const {
@@ -60,7 +76,7 @@ const resolvers = {
         spokenLanguages,
         specialties,
         acceptedInsuranceOptions,
-      } = args.input;
+      } = args;
 
       // TODO: Eventually this should check if a specialty already exists in the DB
       // and match it to that if it does.
