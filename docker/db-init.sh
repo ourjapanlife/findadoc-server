7fa94c7e1ecb19fb2e9149436b8a293706be49de
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2022-12-31 14:44:01 +0900

    docker-compose for the database

--- docker/db-init.sh
+++ docker/db-init.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-
+set -euo pipefail
 echo "😃😃😃😃😃😃DB SETUP TIME😃😃😃😃😃"
 
 echo $POSTGRES_DB
 
-psql -h findadoc-server-db-1 -U $POSTGRES_USER -W $POSTGRES_PASSWORD << EOF
-CREATE DATABASE IF NOT EXISTS $POSTGRES_DB;
-CREATE DATABASE IF NOT EXISTS $SHADOW_DB;
-GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO postgres;
-GRANT ALL PRIVILEGES ON DATABASE $SHADOW_DB TO postgres;
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h database -U $POSTGRES_USER -p $POSTGRES_PORT -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
+
+function create_database_if_not_exists() {
+    local db="$1"
+    PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -p $POSTGRES_PORT -h database -tc "SELECT 1 FROM pg_database WHERE datname = '$db'" | grep -q 1 || \
+    PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -p $POSTGRES_PORT -h database << EOF
+       CREATE DATABASE $db;
+       GRANT ALL PRIVILEGES ON DATABASE $db TO $POSTGRES_USER;
 EOF
+}
+
+create_database_if_not_exists $POSTGRES_DB
+create_database_if_not_exists $SHADOW_DB
+touch out/db-init-done
