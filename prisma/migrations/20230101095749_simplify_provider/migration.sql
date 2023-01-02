da57febc2b6a2ac02ce060c29687c899ddc2ce77
Ann 杏 Kilzer <akilzer@gmail.com>
2023-01-02 19:29:13 +0900

    get rid of old migrations

--- prisma/migrations/20230101095749_simplify_provider/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "HealthcareProfessional" DROP CONSTRAINT "HealthcareProfessional_contactId_fkey";
-
--- AlterTable
-ALTER TABLE "HealthcareProfessional" ALTER COLUMN "contactId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "HealthcareProfessional" ADD CONSTRAINT "HealthcareProfessional_contactId_fkey" FOREIGN KEY ("contactId") REFERENCES "Contact"("id") ON DELETE SET NULL ON UPDATE CASCADE;
