bb2f5d9e5a74ed86c768bc4fbfede8177f5569d3
Ann 杏 Kilzer <akilzer@gmail.com>
2023-01-05 19:12:13 +0900

    configuring which rules make sense for the backend

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,115 @@
 /* eslint-disable-next-line no-undef */
 module.exports = {
-  root: true,
-  extends: [
-    'eslint:recommended',
-    'airbnb-base',
-  ],
-  env: {},
-  globals: {},
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
     extraFileExtensions: [
-      '.json',
-    ],
+      '.json'
+    ]
   },
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-json'
+  ],
+  extends: [
+    'eslint:recommended'
+  ],
+  root: true,
+
+  globals: {},
+
   overrides: [
     {
       files: [
-        '*.ts',
+        '*.ts'
       ],
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'airbnb-base',
-        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended'
       ],
       parserOptions: {
         project: [
-          './tsconfig.json',
-        ],
-      },
-    },
+          './tsconfig.json'
+        ]
+      }
+    }
   ],
-  plugins: [
-    '@typescript-eslint',
-    'eslint-plugin-json',
-  ],
+
+  // 'off' or 0 - turn the rule off
+  // 'warn' or 1 - turn the rule on as a warning (doesn’t affect exit code)
+  // 'error' or 2 - turn the rule on as an error (exit code will be 1)
   rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
+    'block-scoped-var': 'error',
+    complexity: ['error', { max: 40 }],
+    'consistent-return': 'error',
+    curly: 'error',
+    'dot-location': ['error', 'property'],
+    'dot-notation': ['error', { allowPattern: '^[a-z]+(_[a-z]+)+$' }],
+    'no-alert': 'error',
+    'no-multi-spaces': 'error',
+    'no-redeclare': 'error',
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    'vars-on-top': 'off',
+    yoda: ['error', 'never', { exceptRange: true }],
+    'no-console': 'warn', // allow debugging
+
+    // Stylistic Issues and Opinions
+    'arrow-body-style': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-spacing': 'error',
+    'block-spacing': ['error', 'always'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    camelcase: 'error',
+    'comma-dangle': ['error', 'never'],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': 'error',
+    'computed-property-spacing': ['error', 'never'],
+    'function-paren-newline': ['error', 'consistent'],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'linebreak-style': ['error', 'unix'], // no carriage returns
+    'max-len': ['error', 120, 'error', { ignoreUrls: true, ignoreRegExpLiterals: true }],
+    'newline-after-var': 'error',
+    'require-atomic-updates': 'warn', // I'd prefer warn
+    'no-constant-condition': 'error',
+    'no-dupe-class-members': 'error',
+    'no-lonely-if': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }], // let my people push enter!
+    // 'no-prototype-builtins': 'off', // why would we need this off?
+    'no-underscore-dangle': 'error',
+    'no-var': 'error',
+    // 'object-curly-spacing': ['error', 'always'], // always is so wide T________T
+    'object-curly-newline': ['error', { consistent: true }],
+    'object-property-newline': ['error', { allowAllPropertiesOnSameLine: true }],
+    'object-shorthand': ['error', 'methods'],
+    'operator-linebreak': [0, 'before'],
+    'padded-blocks': ['error', 'never'],
+    // 🤨 why do we need more blank lines?
+    // 'padding-line-between-statements': ['error',{ blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+    // { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },{ blankLine: 'always', prev: '*', next: 'return' }],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'quote-props': ['error', 'as-needed'],
+    quotes: ['error', 'single', 'avoid-escape'],
+    'semi-spacing': 'error',
+    'space-before-blocks': 'error',
+    'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+    'space-in-parens': 'error',
+    'space-infix-ops': 'error',
+    semi: ['error'], // personally i like them and it's safer, but can discuss
+
+    // Variables
+    'no-shadow': 'error'
   },
   ignorePatterns: [
-    'src/typeDefs/gqlTypes.ts',
-  ],
+    'src/typeDefs/gqlTypes.ts'
+  ]
 };
