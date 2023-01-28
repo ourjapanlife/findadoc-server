6967c23496894e762ac02252ff8196ee33a4c6a9
Ann 杏 Kilzer <akilzer@gmail.com>
2023-01-28 22:34:22 +0900

    fix tests

--- src/tests/healthcareProfessionalQuery.ts
+++ src/tests/healthcareProfessionalQuery.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
 import { expect } from 'chai';
 import {} from 'mocha';
 import supertest from 'supertest';
@@ -23,28 +25,32 @@ describe('GraphQL', () => {
                 if (err) { return done(err); }
 
                 console.log(res.body);
-                expect(res.body.data.healthcareProfessional).to.have.property('id');
+                expect(res.body.data.healthcareProfessional).to.have.property('id').equal('1');
                 expect(res.body.data.healthcareProfessional).to.have.property('names');
 
                 const { names } = res.body.data.healthcareProfessional;
 
-                expect(names[0]).to.have.property('firstName');
+                expect(names[0]).to.have.property('firstName').equal('Larissa');
                 done();
 
                 return true;
             });
     });
 
-    //   it("Returns all users", (done) => {
-    //     request
-    //       .post("/graphql")
-    //       .send({ query: "{ user { id name username email } }" })
-    //       .expect(200)
-    //       .end((err, res) => {
-    //         // res will contain array of all users
-    //         if (err) return done(err);
-    //         // assume there are a 100 users in the database
-    //         res.body.user.should.have.lengthOf(100);
-    //       });
-    //   });
+    it('Returns all users', done => {
+        request
+            .post('/graphql')
+            .send({ query: '{ healthcareProfessionals { id } }' })
+            .expect(200)
+            .end((err:any, res: any) => {
+                console.log(res.body);
+                // res will contain array of all users
+                if (err) { return done(err); }
+                // assume there are a 100 users in the database
+                expect(res.body.data.healthcareProfessionals).to.have.lengthOf(0);
+                done();
+
+                return true;
+            });
+    });
 });
