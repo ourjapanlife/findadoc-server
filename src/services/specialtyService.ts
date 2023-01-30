331b2b1c12cb453f22872110aa6c14948818b2df
Ann 杏 Kilzer <akilzer@gmail.com>
2023-01-30 22:22:28 +0900

    remove semis

--- src/services/specialtyService.ts
+++ src/services/specialtyService.ts
@@ -1,25 +1,25 @@
 
-import { Specialty, SpecialtyName } from '../typeDefs/gqlTypes';
-import {Specialty as PrismaSpecialty, SpecialtyName as PrismaSpecialtyName} from '@prisma/client';
-import prisma from '../db/client';
+import { Specialty, SpecialtyName } from '../typeDefs/gqlTypes'
+import {Specialty as PrismaSpecialty, SpecialtyName as PrismaSpecialtyName} from '@prisma/client'
+import prisma from '../db/client'
 
 type SpecialtyAndNames = (PrismaSpecialty & { names: PrismaSpecialtyName[]})
 
 function convertPrismaToGraphQLSpecialty(input: SpecialtyAndNames | null) {
-    if (!input) { return null; }
+    if (!input) { return null }
     const ret = {
         id: String(input.id),
         names: Array<SpecialtyName>()
-    } as Specialty;
+    } as Specialty
 
     for (let i = 0; i < input.names.length; i++) {
         ret.names?.push({
             name: input.names[i].name,
             locale: input.names[i].locale
-        });
+        })
     }
 
-    return ret;
+    return ret
 }
 
 // TODO: add a validation step for incoming parameters
@@ -29,22 +29,22 @@ export const getSpecialtyById = async (id: string) => {
     },
     include: {
         names: true
-    }});
+    }})
 
-    return convertPrismaToGraphQLSpecialty(found);
-};
+    return convertPrismaToGraphQLSpecialty(found)
+}
 
 export const getSpecialties = async () => {
     const dbSpecialties = await prisma.specialty.findMany({ include: {
         names: true
-    }});
+    }})
 
-    const gqlSpecialties = Array<Specialty>();
+    const gqlSpecialties = Array<Specialty>()
 
     dbSpecialties.forEach(specialty => {
-        const gqlSpecialty = convertPrismaToGraphQLSpecialty(specialty);
+        const gqlSpecialty = convertPrismaToGraphQLSpecialty(specialty)
 
-        if (gqlSpecialty) { gqlSpecialties.push(gqlSpecialty); }
-    });
-    return gqlSpecialties;
-};
+        if (gqlSpecialty) { gqlSpecialties.push(gqlSpecialty) }
+    })
+    return gqlSpecialties
+}
--- src/tests/degreeQuery.ts
+++ src/tests/degreeQuery.ts
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-import { expect } from 'chai';
-import {} from 'mocha';
-import supertest from 'supertest';
+import { expect } from 'chai'
+import {} from 'mocha'
+import supertest from 'supertest'
 
 describe('GraphQL', () => {
-    let request: any;
+    let request: any
 
     beforeEach(() => {
-        const url = 'http://localhost:3001';
+        const url = 'http://localhost:3001'
 
-        request = supertest(url);
-    });
+        request = supertest(url)
+    })
 
     it('Returns degree with id = 1', done => {
         request
@@ -22,19 +22,19 @@ describe('GraphQL', () => {
             .expect(200)
             .end((err: any, res: any) => {
                 // res will contain array with one user
-                if (err) { return done(err); }
+                if (err) { return done(err) }
 
-                console.log(res.body);
-                expect(res.body.data.degree).to.have.property('id').equal('1');
-                expect(res.body.data.degree).to.have.property('nameJa').equal('医師');
-                expect(res.body.data.degree).to.have.property('nameEn').equal('Medical Doctor');
-                expect(res.body.data.degree).to.have.property('abbreviation').equal('MD');
+                console.log(res.body)
+                expect(res.body.data.degree).to.have.property('id').equal('1')
+                expect(res.body.data.degree).to.have.property('nameJa').equal('医師')
+                expect(res.body.data.degree).to.have.property('nameEn').equal('Medical Doctor')
+                expect(res.body.data.degree).to.have.property('abbreviation').equal('MD')
 
-                done();
+                done()
 
-                return true;
-            });
-    });
+                return true
+            })
+    })
 
     it('Returns all degrees', done => {
         request
@@ -45,14 +45,14 @@ describe('GraphQL', () => {
             .expect(200)
             .end((err: any, res: any) => {
                 // res will contain array with one user
-                if (err) { return done(err); }
+                if (err) { return done(err) }
 
-                console.log(res.body);
-                expect(res.body.data.degrees).to.have.lengthOf(14);
+                console.log(res.body)
+                expect(res.body.data.degrees).to.have.lengthOf(14)
 
-                done();
+                done()
 
-                return true;
-            });
-    });
-});
+                return true
+            })
+    })
+})
