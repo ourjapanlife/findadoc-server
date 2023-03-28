3c33d83892a6539fd4c211f4f8a00028b91da8fd
LaShawn <lts001@gmail.com>
2023-03-28 13:55:56 +0900

    chore: update firebase config with env variables

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,22 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import loadSchema from './schema'
 import resolvers from './resolvers'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSENGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+}
+  
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 const server = new ApolloServer({
     typeDefs: loadSchema(),
