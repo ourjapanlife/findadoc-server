8fe1d4585bc01b2cdde4d21f784665f01b9bf43a
LaShawn Toyoda <31802656+theyokohamalife@users.noreply.github.com>
2023-04-03 21:33:06 +0900

    feat: Add Firebase to the project (#166)
    * chore: add firebase to proj
    
    * chore: update firebase config with env variables
    
    * chore: init firebase project and update docs
    
    * chore: remove dotenv
    
    * chore: comment out firebase app vars
    
    * chore: add updated yarn lock
    
    * updating yarn lock
    
    ---------
    
    Co-authored-by: Philip Ermish <philipermish@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import loadSchema from './schema'
 import resolvers from './resolvers'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore/lite'
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSENGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+}
+  
+// const app = initializeApp(firebaseConfig)
+// const db = getFirestore(app)
+// const analytics = getAnalytics(app)
 
 const server = new ApolloServer({
     typeDefs: loadSchema(),
