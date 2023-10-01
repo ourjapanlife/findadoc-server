d67ca6af54b1655afa9d2c5aa734ba38fda53b6d
theyokohamalife <lts001@gmail.com>
2023-10-01 23:32:12 +0900

    test: add happy path test for updateSubmission

--- __tests__/submissions.test.ts
+++ __tests__/submissions.test.ts
@@ -73,3 +73,83 @@ describe('createSubmission', () => {
         expect(newSubmissionData.id).toBeDefined()
     })
 })
+
+describe('updateSubmission', () => {
+    let url: string
+
+    const server = new ApolloServer({
+        typeDefs: loadSchema(),
+        resolvers
+    })
+
+    beforeAll(async () => {
+        ({ url } = await startStandaloneServer(server, {listen: { port: 0 }}))
+        await initiatilizeFirebaseInstance()
+    })
+
+    afterAll(async () => {
+        await server?.stop()
+    })
+    
+    it('updates a Submission with the fields included in the input', async () => {
+        // Create a new Submission
+        const newSubmission = await request(url).post('/').send(queryData)
+        
+        // Get the ID of the new Submission
+        const submissionId = newSubmission.body.data.createSubmission.id
+
+        // Mutation to update the Submission
+        const submissionQuery = {
+            query: `mutation Mutation($updateSubmissionId: ID!, $input: SubmissionInput) {
+                updateSubmission(id: $updateSubmissionId, input: $input) {
+                  id
+                  googleMapsUrl
+                  healthcareProfessionalName
+                  spokenLanguages {
+                    iso639_3
+                    nameJa
+                    nameEn
+                    nameNative
+                  }
+                  isUnderReview
+                  isApproved
+                  isRejected
+                  createdDate
+                  updatedDate
+                }
+              }`,
+            variables: {
+                input: {
+                    googleMapsUrl: 'http://new.com',
+                    healthcareProfessionalName: 'some NEW name',
+                    spokenLanguages: [
+                        {
+                            iso639_3: 'en',
+                            nameEn: 'English',
+                            nameJa: 'Eigo',
+                            nameNative: 'English'
+                        }
+                    ],
+                    isUnderReview: true,
+                    isApproved: true,
+                    isRejected: true
+                },
+                updateSubmissionId: submissionId
+            }
+        }
+
+        const submission = await request(url).post('/').send(submissionQuery)
+
+        // Compare the data returned in the response to the updated fields that were sent
+        const submissionResponse = submission.body.data.updateSubmission
+        const updatedFields = submissionQuery.variables.input
+
+        expect(submissionResponse.id).toBe(submissionId)
+        expect(submissionResponse.googleMapsUrl).toBe(updatedFields.googleMapsUrl)
+        expect(submissionResponse.healthcareProfessionalName).toBe(updatedFields.healthcareProfessionalName)
+        expect(submissionResponse.spokenLanguages).toEqual(updatedFields.spokenLanguages)
+        expect(submissionResponse.isApproved).toBe(updatedFields.isApproved)
+        expect(submissionResponse.isUnderReview).toBe(updatedFields.isUnderReview)
+        expect(submissionResponse.isRejected).toBe(updatedFields.isRejected)
+    })
+})
