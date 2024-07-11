01b6f3607b03d0346b3480e4b025204da6d5bf1d
Anissa3005 <anissa.chadouli@gmail.com>
2024-07-11 14:41:32 +0900

    chore: add update submission logic to seeding data
    Previously, submissions only included limited data, resulting in some fields being empty. This made frontend development more difficult due to incomplete data. This commit ensures all fields are populated, facilitating easier and more efficient frontend development.

--- utils/databaseSeedTool.ts
+++ utils/databaseSeedTool.ts
@@ -3,9 +3,9 @@ import { createFacility } from '../src/services/facilityService.js'
 
 import { generateRandomCreateHealthcareProfessionalInputArray } from '../src/fakeData/fakeHealthcareProfessionals.js'
 import { generateRandomCreateFacilityInputArray } from '../src/fakeData/fakeFacilities.js'
-import { generateRandomCreateSubmissionInputArray } from '../src/fakeData/submissions.js'
+import { generateRandomCreateSubmissionInputArray, generateRandomUpdateSubmissionInput } from '../src/fakeData/submissions.js'
 import { logger } from '../src/logger.js'
-import { createSubmission } from '../src/services/submissionService.js'
+import { createSubmission, updateSubmission } from '../src/services/submissionService.js'
 
 export const seedDatabase = async () => {
     //eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -15,7 +15,7 @@ export const seedDatabase = async () => {
 
         const facilities = generateRandomCreateFacilityInputArray()
         const healthcareProfessionals = generateRandomCreateHealthcareProfessionalInputArray()
-        const submissions = generateRandomCreateSubmissionInputArray({count: 5})
+        const submissions = generateRandomCreateSubmissionInputArray()
         const facilityIds: string[] = []
 
         for await (const facility of facilities) {
@@ -43,10 +43,18 @@ export const seedDatabase = async () => {
 
         for await (const submission of submissions) {
             const createSubmissionResult = await createSubmission(submission)
-
+            
             //we should fail here if we have errors
             if (createSubmissionResult.hasErrors) {
                 throw new Error(`${JSON.stringify(createSubmissionResult.errors)}`)
+            }
+
+            const updateSubmissionData = generateRandomUpdateSubmissionInput()
+            const updatedSubmissionResult = await updateSubmission(createSubmissionResult.data.id, updateSubmissionData)
+
+            //we should fail here if we have errors
+            if (updatedSubmissionResult.hasErrors) {
+                throw new Error(`${JSON.stringify(updatedSubmissionResult.errors)}`)
             }
         }
     } catch (error) {
