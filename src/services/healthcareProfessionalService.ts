30f58f478785f597ac18d6eafa6aea3af609bb4a
Anissa3005 <anissa.chadouli@gmail.com>
2024-12-05 14:41:47 +0900

    fix: add conditionals to allow updating specific fields

--- src/services/healthcareProfessionalService.ts
+++ src/services/healthcareProfessionalService.ts
@@ -681,8 +681,18 @@ function validateUpdateProfessionalInput(input: Partial<gqlTypes.UpdateHealthcar
         errors: []
     }
 
+    if (Object.keys(input).length < 1) {
+        validationResults.hasErrors = true
+        validationResults.errors?.push({
+            field: 'input',
+            errorCode: ErrorCode.MISSING_INPUT,
+            httpStatus: 400
+        })
+        return validationResults
+    }
+
     //business rule: at least one facility id is required
-    if (!input.facilityIds || input.facilityIds.length < 1) {
+    if (input.facilityIds && input.facilityIds.length < 1) {
         validationResults.hasErrors = true
         validationResults.errors?.push({
             field: 'facilityIds',
@@ -691,11 +701,24 @@ function validateUpdateProfessionalInput(input: Partial<gqlTypes.UpdateHealthcar
         })
     }
 
-    validateNames(input.names, validationResults)
-    validateDegrees(input.degrees, validationResults)
-    validateSpecialties(input.specialties, validationResults)
-    validateInsurance(input.acceptedInsurance, validationResults)
-    validateSpokenLanguages(input.spokenLanguages, validationResults)
+    if (input.names) {
+        validateNames(input.names, validationResults)
+    }
+    if (input.degrees) {
+        validateDegrees(input.degrees, validationResults)
+    }
+
+    if (input.specialties) {
+        validateSpecialties(input.specialties, validationResults)
+    }
+
+    if (input.acceptedInsurance) {
+        validateInsurance(input.acceptedInsurance, validationResults)
+    }
+
+    if (input.spokenLanguages) {
+        validateSpokenLanguages(input.spokenLanguages, validationResults)
+    }
 
     return validationResults
 }
