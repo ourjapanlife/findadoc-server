9e168a77926838875003041d7c3f92756f802695
Matt K <matthew.b.kurtti@gmail.com>
2025-10-02 10:57:07 +0900

    feat: create update user mutation

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -79,3 +79,51 @@ export async function createUser(
     }
 }
 
+/**
+ * Updates a User.
+ * @param input the id of the user and the fields to update
+ * @returns the updated User so you don't have to query it after
+ */
+export async function updateUser(
+    userId: string,
+    fieldsToUpdate: gqlTypes.UpdateUserInput
+): Promise<Result<gqlTypes.User>> {
+    try {
+        // check if user exists and get the array index in the database
+        const userArrayIndex = users.findIndex(u => u.id === userId)
+
+        if (userArrayIndex === -1) {
+            throw new Error('No user exists with the provided id')
+        }
+
+        // update the user using the array index
+        users[userArrayIndex] = {
+            createdDate: users[userArrayIndex].createdDate,
+            id: users[userArrayIndex].id,
+            updatedDate: new Date().toISOString(),
+            displayName: fieldsToUpdate.displayName !== undefined ? fieldsToUpdate.displayName
+                : users[userArrayIndex].displayName,
+            profilePicUrl: fieldsToUpdate.profilePicUrl !== undefined ? fieldsToUpdate.profilePicUrl
+                : users[userArrayIndex].profilePicUrl
+        }
+
+        // return the updated user
+        return {
+            data: users[userArrayIndex],
+            hasErrors: false
+        } 
+    } catch (error) {
+        logger.error(`ERROR: Error creating user: ${error}`)
+
+        return {
+            data: {} as gqlTypes.User,
+            hasErrors: true,
+            errors: [{
+                field: 'createUser',
+                errorCode: ErrorCode.INTERNAL_SERVER_ERROR,
+                httpStatus: 500
+            }]
+        }
+    }
+}
+
